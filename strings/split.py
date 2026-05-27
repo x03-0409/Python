0784266763f1da761268b56552b22787d721f33f
x03-0409 <1243571200@qq.com>
2026-05-27 06:54:55 +0000

    x

--- strings/split.py
+++ strings/split.py
@@ -21,13 +21,17 @@ def split(string: str, separator: str = " ") -> list:
 
     split_words = []
 
+    sep_len = len(separator)
     last_index = 0
-    for index, char in enumerate(string):
-        if char == separator:
-            split_words.append(string[last_index:index])
-            last_index = index + 1
-        if index + 1 == len(string):
-            split_words.append(string[last_index : index + 1])
+    i = 0
+    while i <= len(string) - sep_len:
+        if string[i : i + sep_len] == separator:
+            split_words.append(string[last_index:i])
+            last_index = i + sep_len
+            i = last_index
+            continue
+        i += 1
+    split_words.append(string[last_index:])
     return split_words
 
 
