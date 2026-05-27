24fd2f0fe5d76b3cdbcf4e0fb204b366300d6dde
x03-0409 <1243571200@qq.com>
2026-05-27 07:00:56 +0000

    x

--- strings/split.py
+++ strings/split.py
@@ -1,37 +1,28 @@
-def split(string: str, separator: str = " ") -> list:
-    """
-    Will split the string up into all the values separated by the separator
-    (defaults to spaces)
-
-    >>> split("apple#banana#cherry#orange",separator='#')
-    ['apple', 'banana', 'cherry', 'orange']
-
-    >>> split("Hello there")
-    ['Hello', 'there']
-
-    >>> split("11/22/63",separator = '/')
-    ['11', '22', '63']
-
-    >>> split("12:43:39",separator = ":")
-    ['12', '43', '39']
-
-    >>> split(";abbb;;c;", separator=';')
-    ['', 'abbb', '', 'c', '']
-    """
-
-    split_words = []
-
-    last_index = 0
-    for index, char in enumerate(string):
-        if char == separator:
-            split_words.append(string[last_index:index])
-            last_index = index + 1
-        if index + 1 == len(string):
-            split_words.append(string[last_index : index + 1])
-    return split_words
-
-
-if __name__ == "__main__":
-    from doctest import testmod
-
-    testmod()
+diff --git a/strings/split.py b/strings/split.py
+index ed194ec6..31d1c1d3 100644
+--- a/strings/split.py
++++ b/strings/split.py
+@@ -21,13 +21,17 @@ def split(string: str, separator: str = " ") -> list:
+ 
+     split_words = []
+ 
++    sep_len = len(separator)
+     last_index = 0
+-    for index, char in enumerate(string):
+-        if char == separator:
+-            split_words.append(string[last_index:index])
+-            last_index = index + 1
+-        if index + 1 == len(string):
+-            split_words.append(string[last_index : index + 1])
++    i = 0
++    while i <= len(string) - sep_len:
++        if string[i : i + sep_len] == separator:
++            split_words.append(string[last_index:i])
++            last_index = i + sep_len
++            i = last_index
++            continue
++        i += 1
++    split_words.append(string[last_index:])
+     return split_words
+ 
+ 
